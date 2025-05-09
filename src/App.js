61f1348a8f639e400b3bd8e4d8d7263896a38c9b
Renan Kelm <renan@flexpro.com.br>
2025-05-09 15:43:45 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -152,6 +152,7 @@ export default function Cadastro() {
           isOpen={modalestaaberto2}
           onRequestClose={fecharModal2}
           contentLabel="Modal de exemplo"
+          shouldCloseOnEsc={false}
           className="modal-content"
           overlayClassName="modal-overlay2"
           style={{
@@ -169,9 +170,15 @@ export default function Cadastro() {
             },
           }}
         >
+          <div
+              class="basis1 transition delay-50 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110"
+              id="botaodefechar" style={{marginTop:"10px" , marginLeft:"790px", fontSize:"25px", cursor: "pointer", color:"blue"}}
+            >
+              <span onClick={fecharModal2}>X</span>
+            </div>
           
       <div className="flex flex-col" style={{marginLeft:""}}>
-        <h1 style={{marginLeft:"260px", fontSize:"45px", marginTop:"220px"}}> Resetar Senha</h1>
+        <h1 style={{marginLeft:"260px", fontSize:"45px", marginTop:"190px"}}> Resetar Senha</h1>
         <br />
         <h1 style={{fontSize:"23px", color:"gray", marginLeft:"210px"}}>Digite seu email para resetar sua senha</h1>
         <form onSubmit={handleSubmit} enctype="multipart/form-data" className="flex flex-col items-center">
@@ -181,7 +188,7 @@ export default function Cadastro() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
-            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-black font-thin"
+            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-102 text-black font-thin"
             style={{ marginTop:"40px", marginRight:"10px"}}
           />
           
