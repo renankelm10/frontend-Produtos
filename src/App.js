3c384708c842330a0d7a562425f506bdbded72df
Renan Kelm <renan@flexpro.com.br>
2025-05-09 15:34:17 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Modal, { contextType } from "react-modal";
+import React from "react";
 
 
 export default function Cadastro() {
@@ -16,6 +17,8 @@ export default function Cadastro() {
   const [dados, setDados] = useState([]);
   const [id, setIdSelecionado] = useState('');
   const [modalestaaberto1, setModalAberto1] = React.useState(false);
+  const [modalestaaberto2, setModalAberto2] = React.useState(false);
+  const [mensagem, setMensagem] = useState('');
 
 
   
@@ -28,6 +31,10 @@ export default function Cadastro() {
         setEmail("");
         setSenha("");
         console.log(response)
+        setMensagem("")
+    
+    }).catch ((error) =>  {
+      setMensagem("Email ou Senha incorretos")
     })
   }
 
@@ -79,6 +86,13 @@ export default function Cadastro() {
   function fecharModal() {
     setModalAberto1(false);  
   };
+  function abrirModal2() {
+    setMensagem("")
+    setModalAberto2(true);
+  }
+  function fecharModal2() {
+    setModalAberto2(false);  
+  };
 
   return (
     <div className="App">
@@ -86,13 +100,14 @@ export default function Cadastro() {
       <div className="BackgroundLogin" style={{marginTop:"-800px"}}>
       <div className="flex flex-col" style={{marginLeft:""}}>
         <h1>Login</h1>
+        <h1 style={{fontSize:"17px", color:"red", marginTop:"10px"}}>{mensagem}</h1>
         <form onSubmit={handleSubmit} enctype="multipart/form-data" className="flex flex-col items-center">
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
-            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-black font-mono"
+            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-black font-thin"
             style={{ marginTop:"40px"}}
           />
           <input
@@ -100,10 +115,10 @@ export default function Cadastro() {
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
             placeholder="senha"
-            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-black font-mono"
+            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-black font-thin"
             
           />
-          
+          <h1 onClick={abrirModal2} style={{ marginLeft:"250px",fontSize:"14px", color:"blue", cursor:"pointer"}}>Esqueci Minha Senha</h1>
           <button
             type="submit"
             onClick={(e) => {
@@ -114,6 +129,8 @@ export default function Cadastro() {
           >
             Fazer Login
           </button>
+          <br />
+          
         </form>
         </div>
       </div>
@@ -131,12 +148,69 @@ export default function Cadastro() {
         
         
       </div>
+      <Modal
+          isOpen={modalestaaberto2}
+          onRequestClose={fecharModal2}
+          contentLabel="Modal de exemplo"
+          className="modal-content"
+          overlayClassName="modal-overlay2"
+          style={{
+            overlay: {
+              backgroundColor: "rgba(45, 45, 45, 0.8)",
+            },
+            content: {
+              background: "white",
+              borderRadius: "15px",
+              height: "800px",
+              width: "860px",
+              position: "absolute",
+              marginLeft: "960px",
+              marginTop: "-100px",
+            },
+          }}
+        >
+          
+      <div className="flex flex-col" style={{marginLeft:""}}>
+        <h1 style={{marginLeft:"260px", fontSize:"45px", marginTop:"220px"}}> Resetar Senha</h1>
+        <br />
+        <h1 style={{fontSize:"23px", color:"gray", marginLeft:"210px"}}>Digite seu email para resetar sua senha</h1>
+        <form onSubmit={handleSubmit} enctype="multipart/form-data" className="flex flex-col items-center">
+          <div>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="w-96 py-2 px-4 my-2 text-lg border border-gray-300 rounded-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-black font-thin"
+            style={{ marginTop:"40px", marginRight:"10px"}}
+          />
+          
+          <button
+          style={{marginTop:"20px", borderRadius:"12px"}}
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault(); 
+              login(); 
+            }}
+            className="w-50 py-3 border bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+          >
+            Concluir
+          </button></div>
+          <br />
+          
+        </form>
+        </div>
+      
+      
+
+        </Modal>
 
       <Modal
           isOpen={modalestaaberto1}
           onRequestClose={fecharModal}
           contentLabel="Modal de exemplo"
           className="modal-content"
+          shouldCloseOnEsc={false}
           overlayClassName="modal-overlay"
           style={{
             overlay: {
@@ -144,15 +218,17 @@ export default function Cadastro() {
             },
             content: {
               background: "white",
-              borderRadius: "20px",
-              height: "500px",
-              width: "1000px",
+              borderRadius: "1px",
+              height: "1080px",
+              width: "1920px",
               position: "absolute",
-              marginLeft: "400px",
-              marginTop: "160px",
+              marginLeft: "",
+              marginTop: "-100px",
             },
           }}
         >
+
+        
 
 
 
